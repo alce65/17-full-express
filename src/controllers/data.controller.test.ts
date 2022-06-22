5ccf1072b29faf2ae13ffbc9c38c3a5fbe4cd156
Alejandro Cerezo <alce65@hotmail.es>
2022-06-22 15:37:15 +0200

    Add test for getController

--- src/controllers/data.controller.test.ts
+++ src/controllers/data.controller.test.ts
@@ -4,26 +4,54 @@ import { DataController } from './data.controller';
 
 describe('Given a instantiated controller DataController', () => {
     let dataController: DataController;
-    let req: Request;
+    let req: Partial<Request>;
     let resp: Partial<Response>;
+    let dataModel: DataModel<any>;
     beforeEach(() => {
-        req;
+        req = {
+            params: { id: '1' },
+        };
         resp = {
             setHeader: jest.fn(),
+            status: jest.fn(),
             end: jest.fn(),
         };
         // const dataModel: Partial<DataModel> = {
         //     find: jest.fn(),
         // };
-        const dataModel = new DataModel('test-db');
-        dataModel.findAll = jest.fn();
-
+        dataModel = new DataModel('test-db');
         dataController = new DataController(dataModel);
     });
     describe('When method getAllController is called', () => {
         test('Then resp.end should be called', async () => {
-            await dataController.getAllController(req, resp as Response);
+            dataModel.findAll = jest.fn();
+            await dataController.getAllController(
+                req as Request,
+                resp as Response
+            );
             expect(resp.end).toHaveBeenCalled();
+        });
+    });
+
+    describe('When method getController is called', () => {
+        test('And response is ok, then resp.end should be called with data', async () => {
+            const result = { test: 'test' };
+            dataModel.find = jest.fn().mockResolvedValue(result);
+            await dataController.getController(
+                req as Request,
+                resp as Response
+            );
+            expect(resp.end).toHaveBeenCalledWith(JSON.stringify(result));
+        });
+        test('And response is not ok, then resp.end should be called without data', async () => {
+            const result = null;
+            dataModel.find = jest.fn().mockResolvedValue(result);
+            await dataController.getController(
+                req as Request,
+                resp as Response
+            );
+            expect(resp.end).toHaveBeenCalledWith(JSON.stringify({}));
+            expect(resp.status).toHaveBeenCalledWith(404);
         });
     });
 });
