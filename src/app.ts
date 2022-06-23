07db7c9720ab0a158e0c4285190dc525d59f79a8
Alejandro Cerezo <alce65@hotmail.es>
2022-06-23 11:59:48 +0200

    Middleware & error control

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import morgan from 'morgan';
 // import path from 'path'
 
@@ -7,8 +7,29 @@ import { taskRouter } from './router/task.js';
 
 export const app = express();
 
+// Middlewares
+
 app.use(morgan('dev'));
 app.use(express.json());
 
+export interface ExtraRequest extends Request {
+    calculo: number;
+}
+
+app.use((req, res, next) => {
+    req;
+    res;
+    console.log('Soy un middlewares');
+    (req as ExtraRequest).calculo = 47_638;
+    next();
+});
+
 app.use('/', homeRouter);
 app.use('/task', taskRouter);
+
+app.use((error: Error, req: Request, resp: Response, next: NextFunction) => {
+    req;
+    resp;
+    next;
+    console.log(error.message);
+});
--- src/router/home.ts
+++ src/router/home.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
+import { ExtraRequest } from '../app';
 
 const router = Router();
 
 router.get('/', (req, resp) => {
-    req;
-    resp.end('App Express');
+    const calc = (req as ExtraRequest).calculo;
+    resp.end(`App Express - usuarios ${calc.toLocaleString()}`);
 });
 
 export default router;
